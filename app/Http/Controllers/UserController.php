f53a693cc4ac4de4e75587246fcfd24afb9b91e9
nada-intake40 <n.a.hassan1994@gmail.com>
2020-05-30 19:53:34 +0200

    edit models namespace

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Resources\User as ResourcesUser;
