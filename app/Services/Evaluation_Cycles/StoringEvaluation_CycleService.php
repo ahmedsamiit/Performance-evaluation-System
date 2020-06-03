bac7c1b7b3d7cdbdec441fa42216b021febef500
rola281997 <rolaahmedabdelmaksoud281997@gmail.com>
2020-06-03 18:09:20 +0200

    evaluation_cycle task schedule

--- app/Services/Evaluation_Cycles/StoringEvaluation_CycleService.php
+++ app/Services/Evaluation_Cycles/StoringEvaluation_CycleService.php
@@ -34,16 +34,23 @@ class  StoringEvaluation_CycleService
     public function execute(array $request)
     {
         $start=$request['start'];
-        $dt = Carbon::create($start);
+       $dt = Carbon::create($start);
+
+       $request['start']=$dt;
+       print_r($dt);
         $cycle=$request['cycle'];
 
-        $end=$dt->addMonth($cycle);
+        $end=$dt->addMinute($cycle);
+        //Carbon::parse($request->input('end'));
         $request['end'] = $end;
 
+        $current = Carbon::now();
+        printf($current);
         $evaluation_cycle = $this->repo->create($request);
         if($evaluation_cycle){
             return  response()->json($evaluation_cycle);
         }
+
         return false;
 
     }
